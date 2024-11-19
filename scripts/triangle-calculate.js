e413c74003f8954ec531ffabcf3ebad57a29a017
Nadiasharmin017 <nadiasharmin017@gmail.com>
2024-11-19 17:03:07 +0600

    created rectangle area calculaion

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -17,5 +17,25 @@ function triangleCalculation(){
     const areaspan = document.getElementById("triangle-area");
     areaspan.innerText = area;
     
-    
+}
+
+// 2nd part reactiangle
+
+function areaCalculation2(){
+    // console.log('somthing');
+    const widthId = document.getElementById("width-id");
+    const widthValue = parseFloat(widthId.value) ;
+    console.log(widthValue);
+
+    // length part
+    const lengthId = document.getElementById("length-id");
+    const lengthValue = parseFloat(lengthId.value);
+    console.log(lengthValue);
+
+    // area calculation reactiangle
+    const reactiangleArea = widthValue * lengthValue;
+    // console.log(reactiangleArea);
+    const rectangleCalculationSpan = document.getElementById("rectangle-calculation");
+    rectangleCalculationSpan.innerText = reactiangleArea;
+    console.log(rectangleCalculationSpan);
 }
